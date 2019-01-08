a9ad397dc99ceb9e56eaeca5d873ce600c23f803
michael <zdong2010@gmail.com>
2019-01-08 17:52:06 +0800

    'adonis请求与响应'

--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,38 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+/*
+Route.get('/posts', ({ request}) => {
+  console.log(request.get());
+})
+*/
+// Route.get('/posts', ({ request}) => request.all())
+//Route.get('/posts', ({ request}) => request.only(['title','content']))
+//Route.get('/posts', ({ request}) => request.except(['title','content']))
+//Route.get('/posts', ({ request}) => request.input('status'))
+//Route.post('/posts', ({ request}) => request.collect(['title','content']))
+//Route.get('/posts', ({ request}) => request.headers('adonis-session-values'))
+/*
+const delay = (data,time) => {
+  return new Promise((resolve,reject) => {
+    setTimeout(()=>{
+      resolve(data)
+    },time)
+  })
+}
+Route.get('/posts',async ({ repsonse }) => {
+  const data = await delay('list of post',3000)
+  return data
+})
+*/
+Route.get('/list-of-posts',({ response }) => {
+  //response.redirect('/posts',true,301)
+  response.route('list-of-posts')
+})
+Route.get('/list-of-food-posts',({ response }) => {
+  response.route('list-of-posts',{ category :'food'})
+})
+Route.get('/posts/:category?',({ params })=>{
+  return `List of ${ params.category || 'default'} Posts`
+})
+.as('list-of-posts')
