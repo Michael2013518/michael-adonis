b6b2945f26a522af1b95b8c62d2c6a465096eae7
michael <zdong2010@gmail.com>
2019-01-11 15:59:06 +0800

    '添加querybuilder查询'

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -11,76 +11,66 @@ class PostController {
  }
  async store ({ request, response }) {
     const newPost = request.only(['title', 'content'])
-    const post = await Post.create(newPost)
-    //const tags = request.input('tags')
-    // const postID = await Database.insert(newPost).into('posts')
-    // console.log('postID: ', postID)
-    // const post = await Post.create(newPost)
-
-    // const user = await User.find(request.input('user_id'))
-    // const post = await user
-    //   .posts()
-    //   .create(newPost)
-    //
-    // await post
-    //   .tags()
-    //   .attach(tags)
-
-    return response.redirect(`/posts/${ post.id }`)
+    const postID = await Database.insert(newPost).into('posts')
+    console.log('postID',postID)
+    //const post = await Post.create(newPost)
+    return response.redirect(`/posts/${ postID }`)
   }
  async create ({ view }) {
-    const users = await User.all()
+    // const users = await User.all()
     //const tags = await Tag.all()
-    return view.render('post.create', { users: users.toJSON()})
+    // return view.render('post.create', { users: users.toJSON()})
+    return view.render('post.create')
   }
 
   async show ({ view, params }) {
-    // const post = await Database
-    //   .from('posts')
-    //   .where('id', params.id)
-    //   .first()
+    const post = await Database
+      .from('posts')
+      .where('id', params.id)
+      .first()
 
-    const post = await Post.findOrFail(params.id)
+    // const post = await Post.findOrFail(params.id)
+    //
+    // const tags = await post
+    //   .tags()
+    //   .select('id', 'title')
+    //   .fetch()
 
-    const tags = await post
-      .tags()
-      .select('id', 'title')
-      .fetch()
-
-    return view.render('post.show', { post, tags: tags.toJSON() })
+    //return view.render('post.show', { post, tags: tags.toJSON() })
+    return view.render('post.show', { post })
   }
 
   async edit ({ view, params }) {
-    // const post = await Database
-    //   .from('posts')
-    //   .where('id', params.id)
-    //   .first()
+    const post = await Database
+      .from('posts')
+      .where('id', params.id)
+      .first()
 
-    const post = await Post.findOrFail(params.id)
+    //const post = await Post.findOrFail(params.id)
 
-    return view.render('post.edit', { post: post.toJSON() })
+    return view.render('post.edit', { post })
   }
 
   async update ({ request, params }) {
    const updatedPost = request.only(['title', 'content'])
-   // await Database
-   //   .table('posts')
-   //   .where('id', params.id)
-   //   .update(updatedPost)
+   await Database
+     .table('posts')
+     .where('id', params.id)
+     .update(updatedPost)
 
-   const post = await Post.findOrFail(params.id)
-   post.merge(updatedPost)
-   post.save()
+   // const post = await Post.findOrFail(params.id)
+   // post.merge(updatedPost)
+   // post.save()
  }
 
  async destroy ({ request, params }) {
-    // await Database
-    //   .table('posts')
-    //   .where('id', params.id)
-    //   .delete()
+    await Database
+      .table('posts')
+      .where('id', params.id)
+      .delete()
 
-    const post = await Post.find(params.id)
-    post.delete()
+    // const post = await Post.find(params.id)
+    // post.delete()
 
     return 'success'
   }
